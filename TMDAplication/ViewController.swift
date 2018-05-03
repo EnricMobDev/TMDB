82750bdaf2eac2331209b44d361ebb02820b7ac3
Enric Pou Villanueva <enric.pou.villanueva@everis.com>
2018-05-03 23:41:25 +0200

    UPDATE: Update the storyboard to create the cell to show the different films

--- TMDAplication/ViewController.swift
+++ TMDAplication/ViewController.swift
@@ -21,17 +21,30 @@ struct Films: Decodable {
 class ViewController: UIViewController {
     
     //MARK: - Constants
+    let CELL_IDENTIFIER = "filmCellIdentifier"
     let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=93aea0c77bc168d8bbce3918cefefa45&language=en-US&page=1")
     
     //MARK: - Variables
     var films = [Films]()
-
+    var arrayOfFilms = [Film]()
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
     //MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         alamofireRequest()
     }
 
+    //MARK: - Public methods
+    public func configureUI() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    //MARK: - Private methods
     func alamofireRequest() {
         
         if let secureUrl = url {
@@ -45,13 +58,42 @@ class ViewController: UIViewController {
                             return
                         }
                         self.films = try [JSONDecoder().decode(Films.self, from: dataResponse)]
-                        print (self.films)
+                        
+                        for listOfFilms in self.films {
+                            for eachFilm in listOfFilms.results {
+                                self.arrayOfFilms = listOfFilms.results
+                                print(eachFilm.title, ":", eachFilm.overview, "\n")
+                            }
+                        }
+                        self.tableView.reloadData()
                     }catch {
                         print("error")
                     }
                 }
             }
         }
+    }
+}
+
+//MARK: - UITableViewDelegate, UITableViewDataSource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayOfFilms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        
+        cell = tableView.dequeueReusableCell(withIdentifier: CELL_IDENTIFIER, for: indexPath)
+        
+        
+        if let newCell = cell as? FilmTableViewCell {
+            newCell.bind(film: arrayOfFilms[indexPath.row])
+            cell = newCell
+        }
+        cell.selectionStyle = .none
+        return cell
     }
 }
 
